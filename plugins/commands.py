dd3e1ed008f4de45a9525ea5c628940ee6e9212c
Vysakh <101032366+VysakhTG@users.noreply.github.com>
2023-08-11 05:50:10 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -9,7 +9,7 @@ from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 from database.ia_filterdb import Media, get_file_details, unpack_new_file_id
 from database.users_chats_db import db
 from info import CHANNELS, ADMINS, AUTH_CHANNEL, LOG_CHANNEL, PICS, BATCH_FILE_CAPTION, CUSTOM_FILE_CAPTION, PROTECT_CONTENT
-from utils import get_settings, get_size, is_subscribed, save_group_settings, temp
+from utils import get_settings, get_size, is_requested_one, is_requested_two, save_group_settings, temp
 from database.connections_mdb import active_connection
 import re
 import json
@@ -102,22 +102,22 @@ async def start(client, message):
             )
         return
         
-        if client.link_two is not None and not await is_requested_two(message):
-            btn = [[
-                InlineKeyboardButton(
-                    "🎗 Rᴇǫᴜᴇꜱᴛ Tᴏ Jᴏɪɴ Cʜᴀɴɴᴇʟ 1 🎗", url=client.link_two)
-            ]]
-            try:
-                if client.link_one is not None and not await is_requested_one(message):
-                    btn.append(
-                          [
-                        InlineKeyboardButton(
-                            "🎗 Rᴇǫᴜᴇꜱᴛ Tᴏ Jᴏɪɴ Cʜᴀɴɴᴇʟ 2", url=client.link_one)
-                          ]
-                    )
-            except Exception as e:
-                print(e)
-            if message.command[1] != "subscribe":
+    if client.link_two is not None and not await is_requested_two(message):
+        btn = [[
+            InlineKeyboardButton(
+                "🎗 Rᴇǫᴜᴇꜱᴛ Tᴏ Jᴏɪɴ Cʜᴀɴɴᴇʟ 1 🎗", url=client.link_two)
+        ]]
+        try:
+            if client.link_one is not None and not await is_requested_one(message):
+                btn.append(
+                      [
+                    InlineKeyboardButton(
+                        "🎗 Rᴇǫᴜᴇꜱᴛ Tᴏ Jᴏɪɴ Cʜᴀɴɴᴇʟ 2", url=client.link_one)
+                      ]
+                )
+        except Exception as e:
+            print(e)
+        if message.command[1] != "subscribe":
             try:
                 kk, file_id = message.command[1].split("_", 1)
                 pre = 'checksubp' if kk == 'filep' else 'checksub' 
