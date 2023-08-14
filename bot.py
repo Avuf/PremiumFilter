22c6a71cc67c5ead6d846d07dc017fed256c9bd0
Vysakh <101032366+VysakhTG@users.noreply.github.com>
2023-08-14 06:43:48 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -37,6 +37,8 @@ class Bot(Client):
         b_users, b_chats = await db.get_banned()
         temp.BANNED_USERS = b_users
         temp.BANNED_CHATS = b_chats
+        self.link_one = None
+        self.link_two = None
         await super().start()
         await Media.ensure_indexes()
         me = await self.get_me()
