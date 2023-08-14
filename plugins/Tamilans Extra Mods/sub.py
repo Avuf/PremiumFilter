f6d4e7311b3c4f85eadc9a93a195b8b839f4fd90
Vysakh <101032366+VysakhTG@users.noreply.github.com>
2023-08-14 19:08:19 +0530

    Update sub.py

--- plugins/Tamilans Extra Mods/sub.py
+++ plugins/Tamilans Extra Mods/sub.py
@@ -10,11 +10,11 @@ async def join_reqs(self: Client, join_req: ChatJoinRequest):
     user_id = join_req.from_user.id
     if join_req.chat.id == CHANNEL_ONE:
         try:
-            await db.add_req_one(self, user_id)
+            await db.add_req_one(user_id)
         except Exception as e:
             print(f"Error adding join request to req_one: {e}")
     elif join_req.chat.id == CHANNEL_TWO:
         try:
-            await db.add_req_two(self, user_id)
+            await db.add_req_two(user_id)
         except Exception as e:
             print(f"Error adding join request to req_two: {e}")
